324f597e449642261e08dad2a62bb912180901fb
Daichi Hirose <marumaru09030903@gmail.com>
2025-01-09 19:00:20 +0900

    test.bash

--- test/test.bash
+++ test/test.bash
@@ -5,10 +5,12 @@
 dir=~
 [ "$1" != "" ] && dir="$1"
 
+#!/bin/bash
+dir=~
+[ "$1" != "" ] && dir="$1"
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+cat /tmp/mypkg.log | grep 'Battery:'
 
-cat /tmp/mypkg.log |
-grep 'Listen: 10'
